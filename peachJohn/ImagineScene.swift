5f84f6f820d08786446894fba1e8fd35be9a8844
naoyashiga <naoyashiga0@gmail.com>
2014-11-11 08:31:28 +0900

    人物名の色を試しに赤に変えるアニメーションにした

--- peachJohn/ImagineScene.swift
+++ peachJohn/ImagineScene.swift
@@ -11,24 +11,54 @@ import SpriteKit
 class ImagineScene: SKScene {
     override func didMoveToView(view: SKView) {
         //背景
-        self.backgroundColor = SKColor(red: 200/255.0, green: 200/255.0, blue: 200.0/255.0, alpha: 1.0)
-        let girlMiddleBtn = SKLabelNode(fontNamed:"HelveticaNeue")
-        girlMiddleBtn.text = "AYAKA"
-        girlMiddleBtn.name = "girlMiddle"
-        girlMiddleBtn.fontSize = 50
-        girlMiddleBtn.fontColor = SKColor(red: 0/255.0, green: 0/255.0, blue: 0/255.0, alpha: 1.0)
-        girlMiddleBtn.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        self.backgroundColor = SKColor(red: 255.0, green: 255.0, blue: 255.0, alpha: 1.0)
         
-        addChild(girlMiddleBtn)
-        
+        var girlName:String = "AYAKA"
+        addGirlName(girlName)
         addBackground()
     }
    
+    func addGirlName(name:String){
+        //女の子の名前をセット
+        let girlLabelName = SKLabelNode(fontNamed:"HelveticaNeue")
+        girlLabelName.text = name
+        girlLabelName.fontSize = 50
+        girlLabelName.fontColor = SKColor(red: 0/255.0, green: 0/255.0, blue: 0/255.0, alpha: 1.0)
+        girlLabelName.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        
+        addChild(girlLabelName)
+        
+        let fadeInStart:SKAction = SKAction.fadeInWithDuration(1.5)
+        let fadeOut:SKAction = SKAction.fadeOutWithDuration(1.6)
+        let changeFontColor:SKAction = SKAction.runBlock(
+            {
+                girlLabelName.fontColor = SKColor(red: 255.0, green: 0/255.0, blue: 0/255.0, alpha: 1.0)
+        })
+        let fadeInEnd:SKAction = SKAction.fadeInWithDuration(1.5)
+        
+        let changeFontColorAnimation:SKAction = SKAction.sequence([fadeInStart,fadeOut,changeFontColor,fadeInEnd])
+        
+        girlLabelName.runAction(changeFontColorAnimation)
+        //sprite化
+//        let labelTexture:SKTexture = self.view?.textureFromNode(girlLabelName)
+//        let spriteNode:SKSpriteNode = SKSpriteNode(texture: labelTexture,size:girlLabelName.frame.size)
+//        
+//        spriteNode.anchorPoint = CGPointZero
+//        spriteNode.position = girlLabelName.frame.origin
+//        addChild(spriteNode)
+//        
+//        
+//        let colorAnimation:SKAction = SKAction.colorizeWithColor(SKColor.whiteColor(), colorBlendFactor: 0, duration: 1.0)
+//        
+//        girlLabelName.runAction(colorAnimation)
+        
+        
+    }
     func addBackground(){
         let SCREEN_WIDTH = self.frame.size.width
         let SCREEN_HEIGHT = self.frame.size.height
         let animationDuration = 1.0
-        let bg = SKSpriteNode(color:UIColor.greenColor(),size:CGSizeMake(SCREEN_WIDTH,SCREEN_HEIGHT))
+        let bg = SKSpriteNode(color:UIColor.blackColor(),size:CGSizeMake(SCREEN_WIDTH,SCREEN_HEIGHT))
         bg.position = CGPoint(x: CGRectGetMidX(self.frame),y: -SCREEN_HEIGHT / 2)
         
         addChild(bg)
